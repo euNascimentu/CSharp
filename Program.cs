2fde9e72746910396cde574501db7019138a3812
Fernando <fernando.nprimo@senacsp.edu.br>
2024-06-05 17:21:58 -0300

    Atividades na linguagem CSharp

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿int Idade;
+string nome;
+double num1;
+double num2;
+
+{
+    Console.WriteLine("Olá, por favor digite o seu nome: ");
+    nome = Console.ReadLine();
+
+    Console.WriteLine("Olá " + nome + ", qual sua idade?");
+    Idade = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Digite um numero de 1 a 10: ");
+    num1 = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Legal! Agora digite outro numero de 1 a 10: ");
+    num2 = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Legal a soma de " + num1 + " + " + num2 + " é igual á: " + (num1 + num2));
+    Console.WriteLine("Legal a multiplicação de " + num1 + " * " + num2 + " é igual á: " + (num1 * num2));
+    Console.WriteLine("Legal a divisão de " + num1 + " ÷ " + num2 + " é igual á: " + (num1 / num2));
+    Console.WriteLine("Legal a subtração de " + num1 + " - " + num2 + " é igual á: " + (num1 - num2));
+}
+    
---
+++ if&else.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.Design;
+
+int Idade;
+string Nome;
+
+{
+    Console.WriteLine("Olá, qual seu nome? ");
+    Nome = Console.ReadLine();
+    Console.WriteLine("Olá " + Nome + ". Qual sua idade?: ");
+    Idade = int.Parse(Console.ReadLine());
+
+    if (Idade < 18)
+    {
+        Console.WriteLine("Desculpe, mas só pode entrar maiores de 18 anos.");
+    }
+    else if (Idade >= 18)
+    { 
+        Console.WriteLine("Você ja é maior de idade, pode entrar. Divirta-se e se cuide!"); 
+    }
+}
